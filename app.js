c21883d114bdbdf0cfa2ecc84e930c5cc97d09a0
KWAshen <ashen.maliaha@gmail.com>
2025-08-06 00:53:51 +0200

    ultime modifiche

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = 3000;
 
+const postsrouter = require('./routers/posts');
+
+app.use('/posts',postsRouter);
+
 app.use(express.static("immagini_e_post"));
 
 app.get('/', (req,res) =>{
